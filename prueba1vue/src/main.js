c78037a10f3ddfbabebcc0f781df934088714635
jaume monzonis <jaume_monzonis@hotmail.com>
2020-01-10 12:13:49 +0100

    first commit branch dev

--- prueba1vue/src/main.js
+++ prueba1vue/src/main.js
@@ -26,7 +26,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in.
+    // User is signed in. hhjhjh jfhfghf hgfhgf
     store.dispatch('detectarUsuario',{email: user.email, uid: user.uid})
     // ...
   } else {
